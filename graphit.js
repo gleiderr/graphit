26a49a325542dabbcd548b8b4a408f3ff776083a
Gleider Ribeiro Costa <gleiderr@gmail.com>
2018-07-16 13:13:32 -0300

    Renomeada classe Graphit para Node e alterada ordem dos argumentos de seu construtor; Criada classe Edge;

--- graphit.js
+++ graphit.js
@@ -1,7 +1,7 @@
-export default class Graphit {
-	constructor (obj, id) {
-		this.obj = obj;
+export class Node {
+	constructor (id, obj) {
 		this.id = id;
+		this.obj = obj;
 		this.node = obj[id];
 	}
 
@@ -9,7 +9,7 @@ export default class Graphit {
 		let a = [];
 		if(this.node.content) {
 			for(const id of this.node.content)
-				a.push(new Graphit(this.obj, id));
+				a.push(new Node(id, this.obj));
 		}
 		return a;
 	}
@@ -19,8 +19,19 @@ export default class Graphit {
 		if (this.node.edges){
 			for (let edge of this.node.edges) {
 				let edge_text = edge[0];
-				let node = new Graphit(this.obj, edge[1]);
+				let node = new Node(edge[1], this.obj);
 				es.push({edge_text, node});
+			}
+		}
+		return es;
+	}
+
+	get edges() {
+		let es = [];
+		const edges = this.node.edges;
+		if (edges) {
+			for (let i = 0; i < edges.length; i++) {
+				es.push(new Edge(this.id, edges[i][1], i, this.obj));
 			}
 		}
 		return es;
@@ -40,5 +51,22 @@ export default class Graphit {
 
 	hasEdges() {
 		return Boolean(this.node.edges && this.node.edges.length);
+	}
+}
+
+export class Edge {
+	constructor(from, to, idx, obj) {
+		this.orig = from;
+		this.dest = to;
+		this.i = idx;
+		this.obj = obj;
+	}
+
+	get data() {
+		return this.obj[this.orig].edges[this.i][0];
+	}
+
+	get from() {
+		return new Node(this.orig, this.obj);
 	}
 }
