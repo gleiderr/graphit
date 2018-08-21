976eb0767ad26eed6aeca1c96855800b815b5114
Gleider Ribeiro Costa <gleiderr@gmail.com>
2018-08-21 13:18:26 -0300

    Exclusão de arquivo

--- src/ids.js
+++ src/ids.js
@@ -1,19 +1,4 @@
-export let newId;
 
-export const setup_id = obj => { 
-	let lastID = 0;
-	for (let id in obj) lastID = Math.max(parseInt(id, 36), lastID);
-	console.log(lastID);
 
-	const newIdGenerator = keyGen(lastID, () => 0);
 
-	newId = () => newIdGenerator.next().value;
-};
-
-function* keyGen(init = 0, next = Date.now) {
-	let lastKey = init;
-	while(true) {
-		yield (lastKey = Math.max(next(), lastKey + 1)).toString(36);
-	}
-}
 
