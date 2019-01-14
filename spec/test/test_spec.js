c6912cb3f21128d499a0ad253c17b970375e376e
Gleider Ribeiro Costa <gleiderr@gmail.com>
2019-01-14 11:14:19 -0200

    Escritos testes para Graphit.node();

--- spec/test/test_spec.js
+++ spec/test/test_spec.js
@@ -20,11 +20,12 @@ describe("Graphit integrado ao Firebase", function() {
 
   describe('Graphit.node()', function() {
     describe(', quando não informado [id]', function() {
-      it("e informado [obj], deve retornar instância de GNode com novo [id] e [obj] definidos.", function() {
-        return g.node({ obj: 'teste' })
+      it("e informado [obj], deve retornar instância de GNode com algum novo [id] e [obj] igual ao informado.", function() {
+        const value = 'teste';
+        return g.node({ obj: value })
           .then(obj => {
             expect(obj.id).toEqual(jasmine.anything());
-            expect(obj.obj).toEqual('teste');
+            expect(obj.obj).toEqual(value);
           })
           .catch((error) => fail(error));
       });
@@ -40,8 +41,46 @@ describe("Graphit integrado ao Firebase", function() {
     });
 
     describe(', quando informado [id]', function() {
-      xit('e informado [obj]');
-      xit('e não informado [obj]');
+      it('e informado [obj], deve retornar instância de GNode com [id] e [obj] iguais aos informados.', function() {
+        const value = Math.random();
+        id = 0;
+        return g.node({ id, obj: value })
+          .then(obj => {
+            expect(obj.id).toEqual(id);
+            expect(obj.obj).toEqual(value);
+          })
+          .catch((error) => fail(error));
+      });
+
+      describe('e não informado [obj]', function() {
+        it('(id existente na base), deve retornar instância de GNode com [id] igual ao informado e [obj] igual ao existente na base.', async function() {
+          const value = Math.random();
+          new_obj = await g.node({ obj: value });
+
+          return g.node({ id: new_obj.id })
+            .then(obj => {
+              expect(obj.id).toEqual(new_obj.id);
+              expect(obj.obj).toEqual(value);
+            })
+            .catch((error) => fail(error));;
+        });
+
+        it('(id não existente na base), deve retornar instância de GNode com [id] igual ao informado e [obj] indefinido.', async function() {
+          let new_obj, id;
+          do {
+            id = Math.ceil(Math.random() * 1000);
+            new_obj = await g.node({ id });
+          } while (new_obj.obj != undefined);
+
+          return g.node({ id })
+            .then(obj => {
+              expect(obj.id).toEqual(id);
+              expect(obj.obj).toBeUndefined();
+            })
+            .catch((error) => fail(error));;
+        });
+
+      });
     });
   });
 
